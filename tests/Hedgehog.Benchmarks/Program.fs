6bc3d730a24d89b2b59c09481197ee7c8b0e58fa
Tyson Williams <tyson.williams@blocherconsulting.com>
2021-01-23 14:56:05 -0600

    testing Tree.apply

--- tests/Hedgehog.Benchmarks/Program.fs
+++ tests/Hedgehog.Benchmarks/Program.fs
@@ -44,7 +44,40 @@ type ScaledBenchmarks () =
 
 [<EntryPoint>]
 let main argv =
-    BenchmarkRunner.Run<Benchmarks> () |> ignore
-    BenchmarkRunner.Run<ScaledBenchmarks> () |> ignore
+    //BenchmarkRunner.Run<Benchmarks> () |> ignore
+    //BenchmarkRunner.Run<ScaledBenchmarks> () |> ignore
+
+    let flip f b a = f a b
+    let printTree t =
+      t
+      |> Tree.map (sprintf "%A")
+      |> Tree.render
+      |> printf "%s\n\n"
+
+    let singleA = Tree.singleton "a"
+    let letterChildren =
+      [ Node ("b", [ singleA ])
+        singleA ]
+    let letters = Tree.Node ("c", letterChildren)
+    let single0 = Tree.singleton 0
+    let numberChildren =
+      [ Node (1, [ single0 ])
+        single0 ]
+    let numbers = Tree.Node (2, numberChildren)
+    let createPair a b = (a, b)
+
+    printTree letters
+    printTree numbers
+
+    let apply ta tf =
+      tf |> flip Tree.bind (fun f -> ta |> flip Tree.bind (fun a -> a |> f |> Tree.singleton))
+
+    createPair
+    |> Tree.singleton
+    |> apply letters
+    |> apply numbers
+    |> printTree
+
+    System.Console.ReadKey () |> ignore
 
     0 // return an integer exit code
